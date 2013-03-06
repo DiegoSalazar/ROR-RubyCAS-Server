e85aad158864cef84a88a53fb55a20fb0e0214c4
Marco A. Peraza <marco.a.peraza@dartmouth.edu>
2013-03-06 18:05:39 -0500

    ERB can't take a File, only a string

--- lib/casserver/options_hash.rb
+++ lib/casserver/options_hash.rb
@@ -9,13 +9,13 @@ class OptionsHash < HashWithIndifferentAccess
 	
 	def load_config file
 		load file do |file|
-			merge! HashWithIndifferentAccess.new(YAML.load ERB.new(file).result )[@environment]
+			merge! HashWithIndifferentAccess.new(YAML.load ERB.new(file.read).result )[@environment]
 		end
 	end
 	
 	def load_database_config file
 		load file do |file|
-			self[:database] = HashWithIndifferentAccess.new(YAML.load ERB.new(file).result )[@environment]
+			self[:database] = HashWithIndifferentAccess.new(YAML.load ERB.new(file.read).result )[@environment]
 		end
 	end
 	
